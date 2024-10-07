92c3e3f4c3a7a29762ea7a8e27cda32e07f635ac
Vretinger Vretinger <brozgamers@hotmail.com>
2024-10-07 16:34:40 +0000

    Fix Delete booking modal so it deletes the correct booking

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('manage_bookings/', manage_bookings, name='manage_bookings'),
     path('logout/', custom_logout, name='logout'),
     path('edit/<int:booking_id>/', views.edit_booking, name='edit_booking'),
+    path('delete/<int:booking_id>/', views.delete_booking, name='delete_booking'),
 ]
