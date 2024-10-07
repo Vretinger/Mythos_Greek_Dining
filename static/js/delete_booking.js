2b6da9e9a4c826b3579c89977793fd62aa0c4b4e
Vretinger Vretinger <brozgamers@hotmail.com>
2024-10-07 17:33:44 +0000

    Fix a bug for a second time with deleting a booking where the script dosnt load correct booking ID

--- static/js/delete_booking.js
+++ static/js/delete_booking.js
@@ -1,13 +1,12 @@
- // JavaScript to handle the delete modal
- const deleteModal = document.getElementById('deleteModal');
- deleteModal.addEventListener('show.bs.modal', function (event) {
-     // Get the button that triggered the modal
-     const button = event.relatedTarget; 
-     // Extract the booking ID from the data-* attribute
-     const bookingId = button.getAttribute('data-booking-id'); 
+// JavaScript to handle the delete modal
+const deleteModal = document.getElementById('deleteModal');
+deleteModal.addEventListener('show.bs.modal', function (event) {
+    // Get the button that triggered the modal
+    const button = event.relatedTarget; 
+    // Extract the booking ID from the data-* attribute
+    const bookingId = button.getAttribute('data-booking-id'); 
 
-     // Update the form action URL
-     const form = deleteModal.querySelector('#deleteBookingForm');
-     form.action = `/users/delete_booking/${bookingId}/`;  // Construct the action URL directly
- });
- 
+    // Update the form action URL
+    const form = deleteModal.querySelector('#deleteBookingForm');
+    form.action = `{% url 'delete_booking' '0' %}`.replace('0', bookingId);  // Construct the action URL
+});
