8b122fb2bb017313613036927c0331ea41e9b30c
Vretinger Vretinger <brozgamers@hotmail.com>
2024-10-09 12:54:45 +0000

    Fix correct link to admin interface

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls, name='admin'),
+    path('admin/', admin.site.urls),
     path('users/', include('apps.users.urls')),
     path('bookings/', include('apps.bookings.urls')),
     path('', views.home, name='home'),
