b3537c3a4162efab207c883816ec5ce6e4a42ad2
Vretinger Vretinger <brozgamers@hotmail.com>
2024-10-11 18:12:02 +0000

    Add styling for alert box

---
+++ static/js/close_alerts.js
@@ -0,0 +1,8 @@
+// Auto-dismiss after 5 seconds (5000ms)
+setTimeout(function() {
+    let alerts = document.querySelectorAll('.alert');
+    alerts.forEach(function(alert) {
+        let bsAlert = new bootstrap.Alert(alert);
+        bsAlert.close();
+    });
+}, 5000);
